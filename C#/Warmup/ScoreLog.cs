5801c40fed04e31852a41ba24ab03e5aeec4553a
MJSahebnasi <mjsahebnasi@gmail.com>
2020-08-10 11:27:29 +0430

    Rearrange Repository

--- C#/Warmup/ScoreLog.cs
+++
@@ -1,9 +0,0 @@
-namespace Project02
-{
-    public class ScoreLog
-    {
-        public int StudentNumber { get; set; }
-        public string Lesson { get; set; }
-        public double Score { get; set; }
-    }
-}
--- C#/Warmup/Student.cs
+++
@@ -1,9 +0,0 @@
-namespace Project02
-{
-    public class Student
-    {
-        public int StudentNumber { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
