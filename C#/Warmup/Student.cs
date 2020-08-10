4aafd237429f7eac12d7e908613744e7ba3ef9cf
MJSahebnasi <mjsahebnasi@gmail.com>
2020-08-10 11:30:31 +0430

    C# code

---
+++ C#/Warmup/Student.cs
@@ -0,0 +1,9 @@
+namespace Project02
+{
+    public class Student
+    {
+        public int StudentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
