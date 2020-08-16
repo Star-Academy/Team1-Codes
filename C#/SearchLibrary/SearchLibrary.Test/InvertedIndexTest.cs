8c8442e76c0271d5f8514417092e9d1ca9d791a3
AlirezaT99 <tajmirriahia@yahoo.com>
2020-08-16 13:16:09 +0430

    a minor refactor

--- C#/SearchLibrary/SearchLibrary.Test/InvertedIndexTest.cs
+++ C#/SearchLibrary/SearchLibrary.Test/InvertedIndexTest.cs
@@ -10,14 +10,14 @@ namespace SearchLibrary.Test
         [Fact]
         public void initMapTest()
         {
-            string key = "file";
-            List<string> value = new List<string> { "word1", "word2" };
+            var key = "file";
+            var value = new List<string> { "word1", "word2" };
             inv.InitMap(new Dictionary<string, List<string>> { { key, value } });
-            Dictionary<string, HashSet<string>> expected =
-                    new Dictionary<string, HashSet<string>> {
-                        { "word1", new HashSet<string>{key} },
-                        { "word2", new HashSet<string>{key} }
-                    };
+            var expected = new Dictionary<string, HashSet<string>>
+                {
+                    { "word1", new HashSet<string>{key} },
+                    { "word2", new HashSet<string>{key} }
+                };
             Assert.Equal(expected, inv.Map);
         }
 
