83b3fc45131b96215b6267d53686898b8c7f6d8e
AlirezaT99 <tajmirriahia@yahoo.com>
2020-09-10 11:47:40 +0430

    post method added to api

--- C#/SearchAPI/Controllers/SearchController.cs
+++ C#/SearchAPI/Controllers/SearchController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SearchNest;
@@ -31,12 +32,23 @@ namespace FirstTimeWeb.Controllers
                 : BadRequest(searchResponse.ApiCall.OriginalException.Message);
         }
 
+        [HttpPost]
+        public IEnumerable<Document> GetSearchResult([FromBody] QueryWrapper queryWrapper)
+        {
+            return queryHandler.ProcessQuery(queryWrapper.RawQuery).Documents.ToArray();
+        }
         private ActionResult<string> BuildResult(IEnumerable<Document> responseDocuments)
         {
             return responseDocuments.Any()
-                ? (ActionResult<string>) Ok(
+                ? (ActionResult<string>)Ok(
                     $"Query was found in {responseDocuments.Select(doc => doc.FileName).Aggregate((x, y) => $"{x}, {y}")}")
                 : NotFound("Query wasn't found");
         }
+    }
+
+    public class QueryWrapper
+    {
+        [JsonPropertyName("rawQuery")]
+        public string RawQuery { get; set; }
     }
 }
